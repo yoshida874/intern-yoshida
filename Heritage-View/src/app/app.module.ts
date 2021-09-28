2ddcef74708e58a3fb414af9ef169f93a714a08d
yoshida874 <>
2021-09-29 04:36:53 +0900

    add: 難易度選択をコンポーネント分け

--- Heritage-View/src/app/app.module.ts
+++ Heritage-View/src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// component
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TopComponent } from './pages/top/top.component';
-import { MenuCardsComponent } from './components/menu-cards/menu-cards.component';
 // material
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatRadioModule } from '@angular/material/radio';
-
+// component
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopComponent } from './pages/top/top.component';
+import { MenuCardsComponent } from './components/menu-cards/menu-cards.component';
+import { DifficultySetComponent } from './components/difficulty-set/difficulty-set.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TopComponent,
-    MenuCardsComponent
+    MenuCardsComponent,
+    DifficultySetComponent,
   ],
   imports: [
     BrowserModule,
