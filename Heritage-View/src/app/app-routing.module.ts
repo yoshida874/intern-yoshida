435ff9079748a9c7622abc9d901857f556116b17
yoshida874 <>
2021-10-06 01:36:30 +0900

    add: 解答画面のモックアップ作成

--- Heritage-View/src/app/app-routing.module.ts
+++ Heritage-View/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopComponent } from './pages/top/top.component';
 import { ProblemComponent } from './pages/problem/problem.component';
-import { TopComponent } from './pages/top/top.component'
+import { AnswerComponent } from './pages/answer/answer.component';
 
 const routes: Routes = [
   { path: 'top',  component: TopComponent },
   { path: '', redirectTo: 'top', pathMatch: 'full' },
-  { path: 'problem', component: ProblemComponent }
+  { path: 'problem', component: ProblemComponent },
+  { path: 'answer', component: AnswerComponent},
 ];
 
 @NgModule({
