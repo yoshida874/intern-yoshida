c8d5adc5f332d3f5886e0013add7dc7bc692aef4
yoshida874 <ud8mnzdd@gmail.com>
2021-10-12 13:37:29 +0900

    update: モックデータから名前と解説を取得

--- Heritage-View/src/app/pages/answer/answer.component.ts
+++ Heritage-View/src/app/pages/answer/answer.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface heritage {
+  name: string
+  Commentary: string[]
+}
+
 @Component({
   selector: 'app-answer',
   templateUrl: './answer.component.html',
   styleUrls: ['./answer.component.scss']
 })
 export class AnswerComponent implements OnInit {
-
   nextButtonText = '次の問題へ';
-  constructor() { }
+  heritage: heritage = {
+    name: '古都アユタカ',
+    Commentary: [
+      '1991年に世界遺産に登録',
+      '14世紀に栄えたアユタヤ王朝の遺跡',
+      'ワット・プラ・シーサンペット、ワット・マハータートが有名'
+    ]
+  };
 
+  constructor() { }
   ngOnInit(): void {
   }
 
