a4b8f86577d290370254b4a5904505a14d6d6aad
yoshida874 <>
2021-10-27 03:50:35 +0900

    update: タイマーのカウント開始をmap読み込み後に変更

--- Heritage-View/src/app/pages/problem/problem.component.ts
+++ Heritage-View/src/app/pages/problem/problem.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, timer } from 'rxjs';
+import { timer } from 'rxjs';
 import * as dayjs from 'dayjs';
 
 @Component({
@@ -17,26 +17,20 @@ export class ProblemComponent implements OnInit {
 
   roundTimer = dayjs().minute(0).second(0);
   hintTimer = dayjs().minute(0).second(7);
-  timer$ = timer(1000, 1000);
 
   constructor() {}
 
   ngOnInit(): void {
-    // タイマーを1秒ずつ進める
-    this.timer$.subscribe(() => {
-      if(this.hintTimer.format('mm:ss') !== '00:00'){
-        this.hintTimer = dayjs(this.hintTimer).subtract(1, 's');
-        // ヒントのタイマーが０の時ヒントボタンを有効化
-        if(this.hintTimer.format('mm:ss') === '00:00') this.hintButtonDisabled = false;
-      }
-      this.roundTimer = dayjs(this.roundTimer).add(1, 's');
-    });
+    this.streetViewInit().then(() => this.timerCountInit());
   }
 
-  ngAfterViewInit(): void {
+  /**
+   * streetViewを配置
+   */
+  async streetViewInit(): Promise<void> {
     const option = {
-      addressControl: false,
-      showRoadLabels: false,
+      addressControl: false, // 住所案内を非表示
+      showRoadLabels: false, // 道路名を非表示
       position: {
         lat: 42.345573,
         lng: -71.098326
@@ -46,7 +40,22 @@ export class ProblemComponent implements OnInit {
         pitch: 10,
       }
     }
-    new google.maps.StreetViewPanorama(document.getElementById('pano') as Element, option);
+    await new google.maps.StreetViewPanorama(document.getElementById('pano') as Element, option);
+  }
+
+  /**
+   * ヒント、タイマーのカウントを１秒ずつ進める
+   */
+  timerCountInit(): void {
+    const timer$ = timer(1000, 1000);
+    timer$.subscribe(() => {
+      if(this.hintTimer.format('mm:ss') !== '00:00'){
+        this.hintTimer = dayjs(this.hintTimer).subtract(1, 's');
+        // ヒントのタイマーが０の時ヒントボタンを有効化
+        if(this.hintTimer.format('mm:ss') === '00:00') this.hintButtonDisabled = false;
+      }
+      this.roundTimer = dayjs(this.roundTimer).add(1, 's');
+    });
   }
 
   openHint(): void {
