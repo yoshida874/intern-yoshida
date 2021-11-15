100fae1fd0c6bcbe039506a1dacd05fdc7fa08e0
yoshida874 <>
2021-11-16 01:04:56 +0900

    add: 解答画面にmapを配置

--- Heritage-View/src/app/services/quiz.service.ts
+++ Heritage-View/src/app/services/quiz.service.ts
@@ -1,17 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Heritage } from '../types/heritage';
 
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 const QUIZ_COUNT = 5;
-
-interface Heritage {
-  latitude: number,
-  longitude: number,
-  hint: string[],
-  answer: string[],
-  name: string,
-}
 
 @Injectable({
   providedIn: 'root'
@@ -19,25 +12,25 @@ interface Heritage {
 export class QuizService {
   questionCount: number = 0;
   answerCount: number = 0;
-  answers: string[] = [];
-  heritage: AngularFirestoreCollection<Heritage>;
+  heritage!: Heritage;
+
 
   constructor(
     private router: Router,
     private firestore: AngularFirestore,
-  ) { 
-    this.heritage = this.firestore.collection<Heritage>('heritage', ref => ref.where('name', '==', '古都アユタヤ'));
-    this.heritage.valueChanges().subscribe(item => {
-      this.answers = item[0].answer;
+  ) {
+    const heritageCollection = this.firestore.collection<Heritage>('heritage', ref => ref.where('name', '==', '古都アユタヤ'));
+    heritageCollection.valueChanges().subscribe(item => {
+      this.heritage = item[0];
     });
   }
 
-  getQuiz(): AngularFirestoreCollection<Heritage> {
-    return this.heritage;
+  getQuiz(): Heritage {
+    return this.heritage!;
   }
 
   checkAnswer(inputValue: string): void | boolean {
-    if(this.answers.includes(inputValue)){
+    if(this.heritage.answer.includes(inputValue)){
       ++this.answerCount;
       ++this.questionCount;
       return true;
---
+++ Heritage-View/src/app/types/heritage.ts
@@ -0,0 +1,8 @@
+export interface Heritage {
+  latitude: number,
+  longitude: number,
+  hint: string[],
+  answer: string[],
+  commentary: string[],
+  name: string,
+};
