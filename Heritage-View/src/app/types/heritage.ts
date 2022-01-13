089e46bb308198e0308ef1332d1738bdeb7be9cd
yoshida874 <>
2022-01-13 11:52:57 +0900

    fix: firestoreのフィールド名修正

--- Heritage-View/src/app/types/heritage.ts
+++ Heritage-View/src/app/types/heritage.ts
@@ -1,9 +1,9 @@
 export interface Heritage {
-  latitude: number,
-  longitude: number,
-  hint: string[],
-  answer: string[],
-  commentary: string[],
-  imgUrl: string
-  name: string,
-};
+  latitude: number;
+  longitude: number;
+  hint: string[];
+  answer: string[];
+  commentary: string[];
+  img_url: string;
+  name: string;
+}
